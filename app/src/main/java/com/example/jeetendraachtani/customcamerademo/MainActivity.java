b215d61db5f3d6093fd5c972150b7be6ed0732c0
jeetendra.achtani <jeetendra@eliteinfoworld.com>
2018-03-09 16:18:58 +0530

    added run time permission

--- app/src/main/java/com/example/jeetendraachtani/customcamerademo/MainActivity.java
+++ app/src/main/java/com/example/jeetendraachtani/customcamerademo/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.jeetendraachtani.customcamerademo;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.hardware.Camera;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -17,22 +21,74 @@ public class MainActivity extends AppCompatActivity {
 
     FrameLayout frameLayout;
 
+    final int Req_CAMERA=1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        frameLayout=(FrameLayout)findViewById(R.id.frameLayout);
 
-        //open Camera
+        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
 
-        camera=Camera.open();
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.CAMERA},
+                    Req_CAMERA);
+            return;
+        }
+        initialize();
+    }
 
-        showCamera= new ShowCamera(this,camera);
-        frameLayout.addView(showCamera);
+    private void initialize(){
+        try{
+            frameLayout=(FrameLayout)findViewById(R.id.frameLayout);
+
+            //open Camera
+
+            camera=Camera.open();
+
+            showCamera= new ShowCamera(this,camera);
+            frameLayout.addView(showCamera);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case Req_CAMERA: {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                    initialize();
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(MainActivity.this, "Camera Permission denied", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 
 
